688eb9bcfc46ed356a0589cce54f9d003ba4e651
Mike Whiting <mikejw3@gmail.com>
2021-02-02 16:21:32 +0000

    cleanup

--- src/misc/api.test.js
+++ src/misc/api.test.js
@@ -4,7 +4,6 @@ import { callAPI } from './api';
 describe('API', () => {
   test('Api fetches data', async () => {
     const [results, error] = await callAPI();
-    console.log(results.data.count);
     expect(results.data.count).toEqual(20);
   });
 });
